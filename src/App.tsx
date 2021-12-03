e3133fa764b4071fe0313348908cea8489e882fc
Mathilde <mathildewaerstad@gmail.com>
2021-12-03 13:50:33 +0100

    Fjerne ting som kommer med create react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Lek mer</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          La oss leke litt
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
