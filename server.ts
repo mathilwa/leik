62715bc67c12f8efca1e028245300255d6b061fc
Mathilde <mathildewaerstad@gmail.com>
2022-01-23 23:00:56 +0100

    mer setup

--- server.ts
+++ server.ts
@@ -2,14 +2,14 @@ import path from 'path';
 const express = require('express');
 const app = express();
 
-const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (_req: any, res: any) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+app.get('/', function(_req: any, res: any) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 app.listen(port, () => {
     console.log('Server is up!');
 });
