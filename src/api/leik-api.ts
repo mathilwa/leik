55c3b91128c629a04ccc98d98ac16a51d59e7ea7
Mathilde <mathildewaerstad@gmail.com>
2022-01-26 21:52:42 +0100

    set up local variables

--- src/api/leik-api.ts
+++ src/api/leik-api.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { Game } from '../landing-page/game-types';
 
+const apiUrl = process.env.REACT_APP_LEIK_API_BASEURL;
+
 export const fetchGames = async (): Promise<Game[]> => {
-    const fetchedAuctionObject = await axios.get<Game[]>('https://leik-api.herokuapp.com/');
+    const fetchedAuctionObject = await axios.get<Game[]>(`${apiUrl}/`);
 
     return fetchedAuctionObject.data;
 };
