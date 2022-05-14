65df3f32a110f600f1b6625404c96372a78dd17a
Mathilde <mathildewaerstad@gmail.com>
2022-05-14 11:31:48 +0200

    Sette opp express server

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const publicPath = path.join(__dirname, 'public');
+const publicPath = path.join(__dirname, 'build');
 const port = process.env.PORT || 3000;
 
+console.log('dir', __dirname)
+console.log('public', publicPath)
 app.use(express.static(publicPath));
+// app.use(express.static(__dirname + '/build'));
 
-
-console.log('public',publicPath)
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => {
